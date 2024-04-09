b9c24cde717aea5dbaaf18ab6e8698d01feabfd9
Sebastian Ag <painfulpower96@gmail.com>
2024-04-09 17:38:12 +0200

    first upload

---
+++ app/src/main/java/com/example/itransportthings/InformationActivity.java
@@ -0,0 +1,7 @@
+package com.example.itransportthings;
+
+import androidx.appcompat.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class InformationActivity extends AppCompatActivity {
+}
---
+++ app/src/main/java/com/example/itransportthings/ui/dashboard/DashboardFragment.java
@@ -0,0 +1,39 @@
+package com.example.itransportthings.ui.dashboard;
+
+import android.os.Bundle;
+import android.view.LayoutInflater;
+import android.view.ScaleGestureDetector;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.ImageView;
+import android.widget.TextView;
+
+import androidx.annotation.NonNull;
+import androidx.fragment.app.Fragment;
+import androidx.lifecycle.ViewModelProvider;
+
+import com.example.itransportthings.databinding.FragmentDashboardBinding;
+
+public class DashboardFragment extends Fragment {
+
+    private FragmentDashboardBinding binding;
+
+    public View onCreateView(@NonNull LayoutInflater inflater,
+                             ViewGroup container, Bundle savedInstanceState) {
+        DashboardViewModel dashboardViewModel =
+                new ViewModelProvider(this).get(DashboardViewModel.class);
+
+        binding = FragmentDashboardBinding.inflate(inflater, container, false);
+        View root = binding.getRoot();
+
+        return root;
+    }
+
+
+    @Override
+    public void onDestroyView() {
+        super.onDestroyView();
+        binding = null;
+    }
+
+}
