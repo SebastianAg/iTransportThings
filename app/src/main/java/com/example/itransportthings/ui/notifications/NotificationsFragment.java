1c634f484fa646223519e0e2a179d07172f79d4a
Sebastian Agfemalm <painfulpower96@gmail.com>
2024-04-23 12:16:18 +0200

    Working IP-swap

--- app/src/main/java/com/example/itransportthings/ui/notifications/NotificationsFragment.java
+++ app/src/main/java/com/example/itransportthings/ui/notifications/NotificationsFragment.java
@@ -12,7 +12,9 @@ import android.view.ViewGroup;
 import android.webkit.WebView;
 import android.webkit.WebViewClient;
 import android.widget.Button;
+import android.widget.EditText;
 import android.widget.TextView;
+import android.widget.Toast;
 
 import androidx.annotation.NonNull;
 import androidx.fragment.app.Fragment;
@@ -22,7 +24,9 @@ import com.example.itransportthings.R;
 import com.example.itransportthings.databinding.FragmentNotificationsBinding;
 
 public class NotificationsFragment extends Fragment {
-
+    //private String X = "www.google.com";
+    private String X = "172.20.10.4";
+    EditText ipAddress;
 
     @SuppressLint("SetJavaScriptEnabled")
     public View onCreateView(@NonNull LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState) {
@@ -31,9 +35,42 @@ public class NotificationsFragment extends Fragment {
         WebView webview = view.findViewById(R.id.webView);
         webview.getSettings().setJavaScriptEnabled(true);
         webview.setWebViewClient(new WebViewClient());
-        webview.loadUrl("https://www.youtube.com/watch?v=dQw4w9WgXcQ&t=1s"); //Change this to pico ip
+        webview.loadUrl(X); //Change this to pico ip
+        Button mySwapButton = view.findViewById(R.id.button4);
+        //EditText myEditIP = view.findViewById(R.id.editTextText);
 
+        mySwapButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                AlertDialog.Builder builder = new AlertDialog.Builder(getActivity());
+                builder.setTitle("New IP-address?");
+                ipAddress = new EditText(getActivity());
+                builder.setView(ipAddress);
 
+                builder.setPositiveButton("Submit", new DialogInterface.OnClickListener() {
+                    @Override
+                    public void onClick(DialogInterface dialog, int which) {
+                        X = ipAddress.getText().toString();
+                        webview.loadUrl(X); //Change this to pico ip
+                        //Toast.makeText(getActivity().getApplicationContext(), X, Toast.LENGTH_LONG).show();
+                    }
+                });
+
+                builder.setNegativeButton("Cancel", new DialogInterface.OnClickListener() {
+                    @Override
+                    public void onClick(DialogInterface dialog, int which) {
+                        //dont do anything
+                    }
+                });
+
+                AlertDialog ad = builder.create();
+
+                AlertDialog mDialog = builder.create();
+                mDialog.show();
+            }
+        });
         return view;
     }
+
+
 }
